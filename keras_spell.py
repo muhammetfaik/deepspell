3e8e2383f6305ee4006c6e32d2574da998a7a635
muhammetfaik <muhammetfaik@gmail.com>
2020-03-10 10:35:55 +0300

    line.encode().decode('utf-8')

--- keras_spell.py
+++ keras_spell.py
@@ -373,7 +373,7 @@ def preprocesses_data_clean():
     """Pre-process the data - step 1 - cleanup"""
     with open(NEWS_FILE_NAME_CLEAN, "wb") as clean_data:
         for line in open(NEWS_FILE_NAME):
-            decoded_line = line.decode('utf-8')
+            decoded_line = line.encode().decode('utf-8')
             cleaned_line = clean_text(decoded_line)
             encoded_line = cleaned_line.encode("utf-8")
             clean_data.write(encoded_line + b"\n")
@@ -383,7 +383,7 @@ def preprocesses_data_analyze_chars():
     counter = Counter()
     LOGGER.info("Reading data:")
     for line in open(NEWS_FILE_NAME_CLEAN):
-        decoded_line = line.decode('utf-8')
+        decoded_line = line.encode().decode('utf-8')
         counter.update(decoded_line)
 #     data = open(NEWS_FILE_NAME_CLEAN).read().decode('utf-8')
 #     LOGGER.info("Read.\nCounting characters:")
@@ -408,7 +408,7 @@ def preprocesses_data_filter():
     LOGGER.info("Reading and filtering data:")
     with open(NEWS_FILE_NAME_FILTERED, "wb") as output_file:
         for line in open(NEWS_FILE_NAME_CLEAN):
-            decoded_line = line.decode('utf-8')
+            decoded_line = line.encode().decode('utf-8')
             if decoded_line and not bool(set(decoded_line) - most_popular_chars):
                 output_file.write(line)
     LOGGER.info("Done.")
@@ -433,7 +433,7 @@ def preprocesses_split_lines():
     answers = set()
     with open(NEWS_FILE_NAME_SPLIT, "wb") as output_file:
         for _line in open(NEWS_FILE_NAME_FILTERED):
-            line = _line.decode('utf-8')
+            line = _line.encode().decode('utf-8')
             while len(line) > MIN_INPUT_LEN:
                 if len(line) <= CONFIG.max_input_len:
                     answer = line
@@ -460,7 +460,7 @@ def preprocesses_split_lines2():
     LOGGER.info("Reading filtered data:")
     answers = set()
     for encoded_line in open(NEWS_FILE_NAME_FILTERED):
-        line = encoded_line.decode('utf-8')
+        line = encoded_line.encode().decode('utf-8')
         if CONFIG.max_input_len >= len(line) > MIN_INPUT_LEN:
             answers.add(line)
     LOGGER.info("There are %s 'answers' (sub-sentences)", len(answers))
@@ -477,7 +477,7 @@ def preprocesses_split_lines3():
     LOGGER.info("Reading filtered data:")
     answers = set()
     for encoded_line in open(NEWS_FILE_NAME_FILTERED):
-        line = encoded_line.decode('utf-8')
+        line = encoded_line.encode().decode('utf-8')
         if line.count(" ") < 5:
             answers.add(line)
     LOGGER.info("There are %s 'answers' (sub-sentences)", len(answers))
@@ -498,7 +498,7 @@ def preprocesses_split_lines4():
     print(len(model.wv.index2word))
 #     answers = set()
 #     for encoded_line in open(NEWS_FILE_NAME_FILTERED):
-#         line = encoded_line.decode('utf-8')
+#         line = encoded_line.encode().decode('utf-8')
 #         if line.count(" ") < 5:
 #             answers.add(line)
 #     LOGGER.info("There are %s 'answers' (sub-sentences)", len(answers))
